80fad493627d7ec3e6f8ff76af79c228817b4d6e
Cevdet Baba <92636028+c-baba@users.noreply.github.com>
2023-11-27 10:24:36 +0000

    add text

--- Hello.py
+++ Hello.py
@@ -1,6 +1,8 @@
 import streamlit as st  
 from streamlit_qrcode_scanner import qrcode_scanner  
 
+
+st.write("Please Scan Barcode of Pallet")
 qr_code = qrcode_scanner(key='qrcode_scanner')  
 
 if qr_code:  
